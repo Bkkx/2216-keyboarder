0e986718c66ab932e2bf3ddf25e118b838bd4f35
HongLiang1997 <sia_hongliang@yahoo.com.sg>
2024-06-12 22:05:34 +0800

    Details Page Done

--- keyboarder/productdetails.php
+++ keyboarder/productdetails.php
@@ -10,11 +10,13 @@
         <?php
         include "components/nav.inc.php";
         ?>
-        <main class="container mt-5">
-            <a class="back-button" href="product.php">Back To Product Page</a>
-            <div id="product-details" class="details-container">
-            </div>
+        <main class="container mt-5" id="details-page">
+      
+
         </main>
+        <?php
+        include "components/footer.inc.php";
+        ?>
     </body>
     <link rel="stylesheet" href="css/productdetails.css">
     <script defer src="js/detailcontent.js"></script>
